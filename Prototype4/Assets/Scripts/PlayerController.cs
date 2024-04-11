7c58e55bd4c283a5be2c3d534b12f09b3edec305
karizmyL <142622968+karizmyL@users.noreply.github.com>
2024-04-11 13:17:04 -0700

    removed unused libraries added SerialField

--- Prototype4/Assets/Scripts/PlayerController.cs
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float speed = 5.0f;
-    public GameObject focalPoint;
-    public bool hasPowerup;
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private GameObject focalPoint;
+    [SerializeField] private hasPowerup;
     private float powerUpStrength = 15.0f;
-    public GameObject powerupIndicator;
+    [SerializeField] private GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
