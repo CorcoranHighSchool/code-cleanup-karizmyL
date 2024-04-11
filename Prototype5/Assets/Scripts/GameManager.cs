678f917799000281c90236bee1b189c4fd83ab9e
karizmyL <142622968+karizmyL@users.noreply.github.com>
2024-04-11 13:47:36 -0700

    removed unused libraries added serialize field fixed private set for publicly

--- Prototype5/Assets/Scripts/GameManager.cs
+++ Prototype5/Assets/Scripts/GameManager.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -7,13 +6,13 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     private int score = 0;
-    public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI gameOverText;
-    public GameObject titleScreen;
-    public Button restartButton;
-    public List<GameObject> targets;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI gameOverText;
+    [SerializeField] private GameObject titleScreen;
+    [SerializeField] private Button restartButton;
+    [SerializeField] private List<GameObject> targets;
     private float spawnRate = 1.0f;
-    public bool isGameActive = false;
+    public bool isGameActive = { get; private set; }
 
     public void StartGame(int difficulty)
     {
@@ -25,11 +24,7 @@ public class GameManager : MonoBehaviour
         UpdateScore(0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     private IEnumerator SpawnTarget()
     {
         while (isGameActive)
